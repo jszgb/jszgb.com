a5162c927f690c6c8c492a63f052e6760a67933f
Vedran Blazenka <vedran@reactor.studio>
2018-08-18 20:33:18 +0200

    Updated gatsby-paginate.  Used paginate plugin to create new slides page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,42 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
+const createPaginatedPages = require('gatsby-paginate')
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+        posts: allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            filter: { fileAbsolutePath: { regex: "/(slides)/.*\.md$/" } }
+          ) {
+          totalCount
+          edges {
+            node {
+              id
+              html
+              frontmatter {
+                date(formatString: "DD.MM.YYYY.")
+                title
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      createPaginatedPages({
+        edges: result.data.posts.edges,
+        createPage: createPage,
+        pageTemplate: 'src/templates/slides.js',
+        pathPrefix: 'slides',
+        pageLength: 3
+      })
 
- // You can delete this file if you're not using it
+      resolve()
+    })
+  })
+}
