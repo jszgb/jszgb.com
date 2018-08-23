2150883b431c274a2e0f8b305c7ffb5e96118b41
Vedran Blazenka <blazenka.vedran@gmail.com>
2018-08-23 14:26:46 +0200

    Use single quote instead of backticks

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: `JavaScript Zagreb`
+    title: 'JavaScript Zagreb'
   },
   plugins: [
     'gatsby-plugin-styled-components',
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
-        fonts: [`Oswald`]
+        fonts: ['Oswald']
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `src`,
+        name: 'src',
         path: `${__dirname}/src`
       }
     }
